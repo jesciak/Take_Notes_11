03f7db4a6dd41bfb0b1fb20e2c3e09fe107ff166
jesciak <jesciak@yahoo.com>
2022-10-25 17:42:56 -0400

    change some route code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,48 @@
 
-const router= require('express').Router();
-const api = require('')
+const fs= require('fs');
+const util = require('util');
+const path= require('path');
+const notes= require('express').Router();
+
+const readFromFile= util.promisify(fs.readFile);
+const writeToFile= util.promisify(fs.writeFile);
+
+const readAndAppend= (content,file)=> {
+    fs.readFile(file, 'utf8', (err,data)=>{
+        if(err) {
+            console.log(err);
+        }else{
+            const parsedData= JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        
+        };
+    });
+
+
+
+// const api = require('')
 //GET  Route for note
-router.get('/notes',  (req,res)=>{
-    let noteData=notes;
-   res.json(noteData);
-}
-);
+router.get('/notes', (req,res)=>{
+ readFromFile('./db/db.json').then((data)=>res.json(JSON.parse(data)));
+    });
+
 
 //POST new note
 router.post('/notes,', (req, res)=>{
     console.log(req);
     console.log(req.body);
-    const newNote =req.body;
+    const { title, text} =req.body;
 if (req.body){
-    
+    const newNote ={
+        title,
+        text,
+    };
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully!`);
+}else{
+    res.error(`Error in adding note`);
 }
-    res.status(200)
-   }
-}
-
-app.listen(PORT, function(){
-    console.log("Now Listening");
 });
-
+}
+module.exports= notes;
